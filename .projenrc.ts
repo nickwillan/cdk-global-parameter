c7320bfa71e0a4a111ec4e65ee3deeb6da401ef5
Nick Willan <nickwillan@gmail.com>
2024-07-21 11:03:04 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Nick Willan',
+  authorAddress: 'nickwillan@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.4.0',
+  name: 'cdk-global-parameter',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/nickwillan/cdk-global-parameter.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
