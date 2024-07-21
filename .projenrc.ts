ba4dcf3ca47c3144a0bcd138f7dcb1274901998a
Nick Willan <nickwillan@gmail.com>
2024-07-21 11:13:44 -0400

    Initial Commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Nick Willan',
   authorAddress: 'nickwillan@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.149.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
   name: 'cdk-global-parameter',
